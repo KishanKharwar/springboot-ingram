19130a909aa2506269f51bfbd1b17c7de4f483b8
Kishan.Kharwar <pingmekishan@gmail.com>
2022-05-24 21:42:27 +0530

    [Add] added code for calling the rivt api for multiple times depending on totalItems returned from the api

--- src/main/java/com/oauth2/springoauth/controller/RIVTController.java
+++ src/main/java/com/oauth2/springoauth/controller/RIVTController.java
@@ -29,37 +29,61 @@ public class RIVTController {
 
 	@Autowired
 	private RestTemplate restTemplate;
-	
+
 	@Value("${rivt.api.url}")
 	private String apiUrl;
 	@Value("${rivt.api.basic.auth.username}")
 	private String username;
 	@Value("${rivt.api.basic.auth.password}")
 	private String password;
-	
 
 	@GetMapping("/rivt")
 	@Retryable(HttpServerErrorException.class)
 	public String testRIVTApi() {
-		
-		HttpHeaders h = new HttpHeaders();
-		String s = createJsonRequest();
-		System.out.println("Request in sting : " + s);
+		HttpHeaders h = null;
+		String request = null;
+		HttpEntity<String> entity = null;
+		ResponseEntity<GetRessellerDataResponse> response = null;
+		String totalItems = "";
+		int noOfRequests = 0;
+
+		request = createJsonRequest("1", "100");
+		System.out.println("Request in sting : " + request);
+
+		h = new HttpHeaders();
 		h.setBasicAuth(username, password);
 		h.setContentType(MediaType.APPLICATION_JSON);
 
-		HttpEntity<String> entity = new HttpEntity<>(s, h);
-//		System.out.println(entity);
-		ResponseEntity<GetRessellerDataResponse> response = restTemplate.exchange(apiUrl, HttpMethod.POST, entity, GetRessellerDataResponse.class);
-//		System.out.println(response);
+		entity = new HttpEntity<>(request, h);
+		response = restTemplate.exchange(apiUrl, HttpMethod.POST, entity, GetRessellerDataResponse.class);
+
+		totalItems = response.getBody().getTotalItems();
+
+		if (Integer.valueOf(totalItems) > 100) {
+			noOfRequests = Integer.valueOf(totalItems) + 1;
+
+			for (int i = 2; i <= noOfRequests; i++) {
+				request = createJsonRequest("" + i, "100");
+				entity = new HttpEntity<>(request, h);
+				response = restTemplate.exchange(apiUrl, HttpMethod.POST, entity, GetRessellerDataResponse.class);
+			}
+		}
+
 		return response.getBody().toString();
 	}
 
-	private String createJsonRequest() {
-		ResellerDataEntityRequest entity = new ResellerDataEntityRequest("USA","1","USA","100","INGRAM MICRO");
+	/**
+	 * It creates the string json request to call reseller api with specific pageNumber and pageSize
+	 * @param pageNumber
+	 * @param pageSize
+	 * @return
+	 */
+	private String createJsonRequest(String pageNumber, String pageSize) {
+		ResellerDataEntityRequest entity = new ResellerDataEntityRequest("USA", pageNumber, "USA", pageSize,
+				"INGRAM MICRO");
 		List<ResellerDataEntityRequest> entityList = new ArrayList<>();
 		entityList.add(entity);
-		GetRessellerDataRequest request = new GetRessellerDataRequest("getRessellerData","3","IMDISTI",entityList);
+		GetRessellerDataRequest request = new GetRessellerDataRequest("getRessellerData", "3", "IMDISTI", entityList);
 		String json = new Gson().toJson(request);
 		return json;
 	}
